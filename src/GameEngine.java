16f4b3f424aedb24e163d887334b21ab4d566c13
Carson <ctanne14@asu.edu>
2025-11-10 19:50:23 -0700

    Updated commit message including all files

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -17,3 +17,18 @@ board[i][j] = '-';
 }
 //TODO: Implement game logic methods
 }
+public boolean makeMove(int row, int col){
+if(row < 0 || row >= 3 || col < 0 || col >= 3){
+return false; //invalid position
+}
+if(board[row][col]!= '-'){
+return false; //Position already taken
+}
+board[row][col] = currentPlayer;
+return true;
+}
+
+public void switchPlayer(){
+currentPLayer = (currentPLayer == 'X')?'O' : 'X';
+}
+}

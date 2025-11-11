1ae6976d3460694144f06e76965d519c8748c24a
Carson <ctanne14@asu.edu>
2025-11-10 17:34:47 -0700

    Add initial TicTacToe class with main method

---
+++ README.md
@@ -0,0 +1,10 @@
+A simple TicTacToe game implementation in Java.
+## Features
+- COnsole-based gameplay
+- Two-player support
+- Win detection
+
+## How to run
+'''bash
+javac src/TicTacToe.java
+java -cp src TicTacToe
---
+++ src/TicTacToe.java
@@ -0,0 +1,10 @@
+public class TicTacToe{
+public static void main(String[] args){
+cat > src/TicTacToe.java << 'EOF'
+public class TicTacToe{
+public static void main(String[] args){
+
+System.out.println("Welcome to TicTacToe!");
+//Todo: Implement game logic
+}
+}
